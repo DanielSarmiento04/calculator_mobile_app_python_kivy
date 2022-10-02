0b6dfdc38021e4296dc19fd7ef319a13825b21f6
Daniel <josedanielsarmiento219@gmail.com>
2022-10-02 16:46:33 -0500

    Add control symbol funcionality

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-from utils import get_grid, get_zero, is_number, get_operation_symbol, get_control_symbol
+from utils import get_grid, get_zero, is_number, verify_operation_symbol, verify_control_symbol, verify_special_symbol
 
 class Calculator(App):
     def build(self):
         self.window = GridLayout()
         # put the number of columns
         self.window.cols = 4
-        #put the result label in the first row
-        self.result_label = Label(text="0", size_hint=(1, .5))
+        #put the result label in the first row and extend it to 4 columns
+        self.result_label = Label(text="0", font_size=40, size_hint=(1, .5), halign="right", valign="top")
         self.window.add_widget(self.result_label)
         
         # Add a button
@@ -21,31 +21,48 @@ class Calculator(App):
             for item in row:
                 # set gray color for the buttons
                 background_color = (0.5, 0.5, 0.5, 1)
-                if get_operation_symbol(item):    
-                    # put the operation buttons in orange color 
+                if verify_operation_symbol(item):    
+                    # Put the operation buttons in orange color 
                     background_color = "#FFAA00"
+                
+                # Create the button 
                 button = Button(text=item, font_size=40, 
                             background_color=background_color, 
                             bold=True,
-                            background_normal='', # remove the default background_color of the button when pressed       
+                            # normalize the bacground color
+                            background_normal='', 
                             )    
                 button.bind(on_press=self.callback_for_button)
-                # button.center = self.window.center
+                # add the button to the window
                 self.window.add_widget(button)  
-                # self.window.add_widget(Button(text=item, font_size=50))
+
         return self.window
 
     def callback_for_button(self, instance):
         """ Callback for button press """
         # When the first number is pressed, change the result_label,
         # separe the number from the operation symbol and the control symbol
-        if self.result_label.text == "0" and (not get_operation_symbol(instance.text)) and not get_control_symbol(instance.text):
+        if self.result_label.text == "0" and (not verify_operation_symbol(instance.text)) and not verify_control_symbol(instance.text) and not verify_special_symbol(instance.text):
             print("First number pressed")
             self.result_label.text = instance.text
         # When press another number, add the number to the result_label
-        elif self.result_label.text != "0" and (not get_operation_symbol(instance.text)) and not get_control_symbol(instance.text):
+        elif self.result_label.text != "0" and (not verify_operation_symbol(instance.text)) and not verify_control_symbol(instance.text):
+            print('Another number pressed and comma')
             self.result_label.text += instance.text
-        elif get_operation_symbol(instance.text):
+        elif verify_control_symbol(instance.text):
+            print('Control symbol pressed')
+            if instance.text == "AC":
+                self.result_label.text = "0"
+            elif instance.text == "+/-":
+                if self.result_label.text[0] == "-":
+                    self.result_label.text = self.result_label.text[1:]
+                else:
+                    self.result_label.text = "-" + self.result_label.text
+            elif instance.text == "%":
+                self.result_label.text = str(float(self.result_label.text)/100)
+
+
+        elif verify_operation_symbol(instance.text):
             pass
 
 
--- utils.py
+++ utils.py
@@ -4,9 +4,17 @@ first_row = np.array(['AC',"+/-","%","/"], dtype=object)
 second_row = np.array(['7','8','9','x'], dtype=object)
 third_row = np.array(['4','5','6','-'], dtype=object)
 fourth_row = np.array(['1','2','3','+'], dtype=object)
-fifth_row = np.array(['0','.','='], dtype=object)
+fifth_row = np.array(['0',',','='], dtype=object)
 operaction_symbols = np.array(['+','-','x','/'], dtype=object)
 control_symbols = np.array(['AC','+/-','%'], dtype=object)
+special_symbols = np.array([',','='], dtype=object)
+
+def verify_special_symbol(value) -> bool:
+    """Return the special symbol"""
+    if value in special_symbols:
+        return True
+    else:
+        return False
 
 def get_zero() -> str:
    """ Return the zero """
@@ -24,14 +32,14 @@ def is_number(value) -> bool:
     except ValueError:
         return False
 
-def get_operation_symbol(value) -> bool:
+def verify_operation_symbol(value) -> bool:
     """Return the operation symbol"""
     if value in operaction_symbols:
         return True
     else:
         return False
 
-def get_control_symbol(value) -> bool:
+def verify_control_symbol(value) -> bool:
     """Return the control symbol"""
     if value in control_symbols:
         return True
