95b9300d3ce65542ff0bfcf6830a1b5771fe62ea
Daniel <josedanielsarmiento219@gmail.com>
2022-10-02 17:56:17 -0500

    beta 1

--- main.py
+++ main.py
@@ -47,31 +47,62 @@ class Calculator(App):
         if self.result_label.text == "0" and (not verify_operation_symbol(instance.text)) and not verify_control_symbol(instance.text) and not verify_special_symbol(instance.text):
             print("First number pressed")
             self.result_label.text = instance.text
+            self.aux_result_variable += instance.text
+
         # When press another number, add the number to the result_label
         elif self.result_label.text != "0" and (not verify_operation_symbol(instance.text)) and not verify_control_symbol(instance.text)  and not verify_special_symbol(instance.text):
-            print('Another number pressed and comma')
+            print('Another number pressed')
             self.result_label.text += instance.text
+            self.aux_result_variable += instance.text
+
+        # When press control symbol, change the result_label
         elif verify_control_symbol(instance.text):
             print('Control symbol pressed')
+            # Clear the label
             if instance.text == "AC":
                 self.result_label.text = "0"
+                self.aux_result_variable = ""
+            # Change the signal of the number
             elif instance.text == "+/-":
                 if self.result_label.text[0] == "-":
                     self.result_label.text = self.result_label.text[1:]
+                    self.aux_result_variable = self.aux_result_variable[1:]
                 else:
                     self.result_label.text = "-" + self.result_label.text
+                    self.aux_result_variable = "-" + self.aux_result_variable
+            # Divide the number by 100
             elif instance.text == "%":
                 self.result_label.text = str(float(self.result_label.text)/100)
+                self.aux_result_variable = self.result_label.text
 
+        elif verify_operation_symbol(instance.text):
+            print('Operation symbol pressed')
+            # When the opreation symbol is pressed, add the opreation to the aux_result_variable and refresh the result_label
+            if instance.text == "+":
+                self.aux_result_variable += "+"
+                self.result_label.text = "0"
+            elif instance.text == "-":
+                self.aux_result_variable += "-"
+                self.result_label.text = "0"
+            elif instance.text == "x":
+                self.aux_result_variable += "*"
+                self.result_label.text = "0"
+            elif instance.text == "/":
+                self.aux_result_variable += "/"
+                self.result_label.text = "0"
 
         elif verify_special_symbol(instance.text):
             print('Special symbol pressed')
             if instance.text == ",":
                 # Verify if the number already has a comma if not add only one
                 if not "," in self.result_label.text:
-                    self.result_label.text += instance.text
+                    self.result_label.text += ","
+                    self.aux_result_variable += "."
             elif instance.text == "=":
-                self.result_label.text = str(eval(self.result_label.text))
+                print("Equal pressed")
+                print(self.aux_result_variable)
+                self.result_label.text = str(eval(self.aux_result_variable)).replace(".", ",")
+                self.aux_result_variable = self.result_label.text.replace(",", ".")
 
 
 Calculator().run()
